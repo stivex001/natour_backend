6ce533e7fd94e790f22e081ff571926584286b29
stivex001 <stephenadeyemo@gmail.com>
2025-09-13 16:17:12 +0100

    feat:added dot env for setting the node env to either development or production

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const app = require("./index")
+const dotenv = require("dotenv");
+dotenv.config();
+const app = require("./index");
 
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+console.log(process.env.NODE_ENV)
 
 app.listen(port, () => {
   console.log(`App running on Port ${port}`);
