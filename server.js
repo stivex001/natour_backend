54f661cefd142aaeb45b353d95fe1508c58b46a1
stivex001 <stephenadeyemo@gmail.com>
2025-09-14 22:23:48 +0100

    feat:connected to mongo db successfully

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = require("./index");
 
-
 const port = process.env.PORT || 3000;
-console.log(process.env.NODE_ENV)
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to DB Successfully"))
+  .catch((err) => console.log("Couldn't connect to MongoDB", err));
 
 app.listen(port, () => {
   console.log(`App running on Port ${port}`);
