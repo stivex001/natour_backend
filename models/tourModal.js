f10801dff6cd4c0c82a68246c31b7ae4475e8151
stivex001 <stephenadeyemo@gmail.com>
2025-09-16 14:37:33 +0100

    feat:added new schema to the tour endpoint

--- models/tourModal.js
+++ models/tourModal.js
@@ -5,6 +5,19 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "Tour name cannot be empty"],
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
   },
   price: {
     type: Number,
@@ -14,8 +27,38 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     default: 4.5,
   },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a description"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
-const Tour = mongoose.model("Tour", tourSchema)
+const Tour = mongoose.model("Tour", tourSchema);
 
-module.exports = Tour
+module.exports = Tour;
