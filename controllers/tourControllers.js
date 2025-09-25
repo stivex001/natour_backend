49c5d6db12c28db36084967f7d34495af2c2153a
stivex001 <stephenadeyemo@gmail.com>
2025-09-25 12:50:26 +0100

    feat: update getTourStats to group by difficulty and aggregate tour statistics

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -72,13 +72,18 @@ exports.getTourStats = async (req, res) => {
       },
       {
         $group: {
-          _id: null,
+          _id: "$difficulty",
+          numTours: { $sum: 1 },
+          numRatings: {$sum: "$ratingsQuantity"},
           avgRating: { $avg: "$ratingsAverage" },
           avgPrice: { $avg: "$price" },
           minPrice: { $min: "$price" },
           maxPrice: { $max: "$price" },
         },
       },
+      {
+        $sort: {avgPrice: 1}
+      }
     ]);
 
     res.status(200).json({
