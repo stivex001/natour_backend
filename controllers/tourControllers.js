39f1caa10a54a7e44a6e382d3591d7d15bcad59e
stivex001 <stephenadeyemo@gmail.com>
2025-09-25 11:22:15 +0100

    feat: implement ApiFeatures class for query handling in getAllTours

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,4 +1,5 @@
 const Tour = require("../models/tourModal");
+const ApiFeatures = require("../utils/apiFeatures");
 
 exports.createTour = async (req, res) => {
   try {
@@ -20,47 +21,12 @@ exports.createTour = async (req, res) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    const filter = JSON.parse(queryStr);
-
-
-    let query = Tour.find(filter);
-
-    // Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    // Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields)
-    } else {
-      query = query.select("-__v");
-    }
-
-    // Pagination
-    const page = req.query.page * 1 || 1
-    const limit = req.query.limit * 1 || 100
-    const skip = (page - 1) * limit
-
-    query = query.skip(skip).limit(limit)
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments()
-      if (skip >= numTours) throw new Error("This page does not exist")
-    }
-
-    const tours = await query;
+    const features = new ApiFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limit()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: "success",
