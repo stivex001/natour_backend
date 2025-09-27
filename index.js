f12bb5bd4d3418fd38b6623c3ef230f12fe3ef3c
stivex001 <stephenadeyemo@gmail.com>
2025-09-27 18:24:07 +0100

    refactor: comment out 404 error handling middleware in index.js

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.use(express.json());
 
 app.use("/api/v1", routes);
 
-module.exports = app
+// app.all("*", (req, res, next) => {
+//   res.status(404).json({
+//     status: "fail",
+//     message: `Can't find ${req.originalUrl} on this server!`,
+//   });
+//   next()
+// });
 
-
+module.exports = app;
