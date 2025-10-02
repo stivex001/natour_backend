2e1bad0eeb8f5dd8ddb8bda5687b7d825a259e37
stivex001 <stephenadeyemo@gmail.com>
2025-10-03 00:06:38 +0100

    feat: implement user login functionality and enhance user model with password comparison method

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,15 @@
-const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
+const User = require("../models/userModel");
+const AppError = require("../utils/appError");
+
 const catchAsync = require("../utils/catchAsync");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.register = catchAsync(async (req, res, next) => {
   const { name, email, password, confirmPassword } = req.body;
@@ -13,9 +21,7 @@ exports.register = catchAsync(async (req, res, next) => {
     confirmPassword,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: "success",
@@ -23,5 +29,28 @@ exports.register = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  const confirmPassword = await user.correctPassword(password, user.password);
+
+  if (!user || !confirmPassword) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+
+  const token = signToken(user._id);
+
+  return res.status(200).json({
+    status: "success",
+    access_token: token,
+    user,
   });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -24,6 +24,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "User password is required"],
       minlength: 8,
+      select: false,
     },
     confirmPassword: {
       type: String,
@@ -50,6 +51,11 @@ userSchema.pre("save", async function (next) {
 
   next();
 });
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+
+  return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model("User", userSchema);
 
