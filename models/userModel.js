3d248d74d23a0dd2351d127b61ea8eff6aeab1f7
stivex001 <stephenadeyemo@gmail.com>
2025-10-02 01:10:06 +0100

    feat: add password hashing and confirmation validation to user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -27,12 +28,28 @@ const userSchema = new mongoose.Schema(
     confirmPassword: {
       type: String,
       required: [true, "Please confirm your password"],
+      validate: {
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "Passwords do not match",
+      },
     },
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.confirmPassword = undefined;
+
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
