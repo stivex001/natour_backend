7550741c8df037b1a2cbb1e629143c888a8f42f1
stivex001 <stephenadeyemo@gmail.com>
2025-10-15 20:17:21 +0100

    feat:integrated forgot password

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+const sendEmail = require("../utils/email");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -78,7 +79,6 @@ exports.protectedRoute = catchAsync(async (req, res, next) => {
     );
   }
 
-
   // Check if user changed password after the token was issued
   // if (currentUser.changePasswordAfter(decoded.iat)) {
   //   return next(
@@ -109,4 +109,37 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
+
+  // Send it to user's email
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/auth/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a request with your new password and confirmPassword to: ${resetUrl}.\nIf you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Your password reset token (valid for 10 mins)",
+      message,
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Token sent to email!",
+    });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError(
+        "There was an error sending the email. Try again later!",
+        500
+      )
+    );
+  }
 });
+
+exports.resetPassword = catchAsync(async (req, res, next) => {});
--- models/userModel.js
+++ models/userModel.js
@@ -85,7 +85,7 @@ userSchema.methods.changePasswordAfter = function (JWTTimestamp) {
 userSchema.methods.createPasswordResetToken = function () {
   const resetToken = crypto.randomBytes(32).toString("hex");
 
-  this.passwordResetToken = crypto.hash("sha256").update(resetToken).digest("hex");
+  this.passwordResetToken = crypto.createHash("sha256").update(resetToken).digest("hex");
 
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
